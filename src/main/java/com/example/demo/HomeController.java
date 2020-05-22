4158c62528f5f3304c3e453e92bb5c70a739027e
azdoolittle <drewdoolittle@gmail.com>
2020-05-22 16:49:11 -0400

    fixed title property error

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.text.Bidi;
 
 @Controller
 public class HomeController {
